495d5f0d3faa1dea176319477e0da59e37b5bbb6
skamo3 <joochan123123@gmail.com>
2022-11-06 22:48:00 +0100

    [Feat] ChessGameMode create
    Init chess game mode class

---
+++ Source/ChessProject/Public/ChessGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ChessGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHESSPROJECT_API AChessGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
