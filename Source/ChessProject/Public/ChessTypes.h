0e69efc13c80f15b146a1bdc6e45dcaa4fbaeeea
skamo3 <joochan123123@gmail.com>
2023-02-20 18:24:04 +0100

    Add types

--- Source/ChessProject/Public/ChessTypes.h
+++ Source/ChessProject/Public/ChessTypes.h
@@ -1,37 +1,78 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine/DataTable.h"
 #include "ChessTypes.generated.h"
 
-UENUM()
-enum class EChessRank
+UENUM(BlueprintType)
+enum class EChessRank : uint8
 {
-	Rank1,
-	Rank2,
-	Rank3,
-	Rank4,
-	Rank5,
-	Rank6,
-	Rank7,
-	Rank8,
+	Rank1	UMETA(DisplayName = "Rank1"),
+	Rank2	UMETA(DisplayName = "Rank2"),
+	Rank3	UMETA(DisplayName = "Rank3"),
+	Rank4	UMETA(DisplayName = "Rank4"),
+	Rank5	UMETA(DisplayName = "Rank5"),
+	Rank6	UMETA(DisplayName = "Rank6"),
+	Rank7	UMETA(DisplayName = "Rank7"),
+	Rank8	UMETA(DisplayName = "Rank8"),
+	MaxNum	UMETA(DisplayName = "MaxRank"),
 };
 
-UENUM()
-enum class EChessFile
+UENUM(BlueprintType)
+enum class EChessFile : uint8
 {
-	FileA,
-	FileB,
-	FileC,
-	FileD,
-	FileE,
-	FileF,
-	FileG,
-	FileH,
+	FileA	UMETA(DisplayName = "FileA"),
+	FileB	UMETA(DisplayName = "FileB"),
+	FileC	UMETA(DisplayName = "FileC"),
+	FileD	UMETA(DisplayName = "FileD"),
+	FileE	UMETA(DisplayName = "FileE"),
+	FileF	UMETA(DisplayName = "FileF"),
+	FileG	UMETA(DisplayName = "FileG"),
+	FileH	UMETA(DisplayName = "FileH"),
+	MaxNum	UMETA(DisplayName = "MaxFile"),
 };
 
-UENUM()
-enum class EChessTeam
+UENUM(BlueprintType)
+enum class EChessTeam : uint8
 {
-	Black,
-	White,
+	Black	UMETA(DisplayName = "Black"),
+	White	UMETA(DisplayName = "White"),
+	MaxNum	UMETA(DisplayName = "MaxNum"),
+};
+
+UENUM(BlueprintType)
+enum class EChessType : uint8
+{
+	Pawn	UMETA(DisplayName = "Pawn"),
+	Rook	UMETA(DisplayName = "Rook"),
+	Knight	UMETA(DisplayName = "Knight"),
+	Bishop	UMETA(DisplayName = "Bishop"),
+	Queen	UMETA(DisplayName = "Queen"),
+	King	UMETA(DisplayName = "King"),
+	MaxNum	UMETA(DisplayName = "MaxNum"),
+};
+
+USTRUCT(Atomic, BlueprintType)
+struct FChessDataRow : public FTableRowBase
+{
+
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Init")
+		FVector Location;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Init")
+		EChessType Type;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Init")
+		EChessTeam Team;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Init")
+		EChessRank Rank;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Init")
+		EChessFile File;
+
+
 };
